b361bf1ef64e785a9bd0729826cde3e8ccca8d49
itshanna01 <potatoogracee@gmail.com>
2024-11-22 15:05:11 +0800

    Create Bicycle.java

---
+++ Bicycle/Bicycle.java
@@ -0,0 +1,31 @@
+//Super class
+
+class Bicycle {
+  //the Bicycle class has two fields
+  public int gear;
+  public int speed;
+
+  //the Bicycle class has one constructor
+  public Bicyle(int gear, int speed)
+  {
+    this.gear = gear;
+    this.speed = speed;
+  }
+
+  //the Bicycle class has three methods
+  public void applyBrake(int decrement)
+  {
+    speed -= decrement;
+  }
+
+  public void speedUp(int increment)
+  {
+    speed += increment;
+  }
+
+  public String toString()
+  {
+    return ("No of gears are " + gear + "\n"
+            + "speed of bicycle is " + speed);
+  }
+}
